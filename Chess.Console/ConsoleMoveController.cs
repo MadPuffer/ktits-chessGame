485aefeccc2fab823feeb0618cdb188916e4fcf9
Bulat <kotdusovb@gmail.com>
2022-06-13 18:50:59 +0300

    Add Chess.Console to solution

---
+++ Chess.Console/ConsoleMoveController.cs
@@ -0,0 +1,24 @@
+﻿using Chess.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Console
+{
+    public class ConsoleMoveController : IMoveController<String>
+    {
+        public void Move(Piece<string> piece, string destCell)
+        {
+            // 64 - magic digit to cast char into int in correct form
+
+            if (piece.IsRightMove(piece.Col, piece.Row, destCell[0] - 64, int.Parse(destCell[1].ToString())))
+            {
+                piece.Col = destCell[0] - 64;
+                piece.Row = int.Parse(destCell[1].ToString());
+                piece.Cell = destCell;
+            }
+        }
+    }
+}
--- Chess.Console/Program.cs
+++ Chess.Console/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Chess.Core;
+using Chess.Console;
+
+ConsoleMoveController moveController = new ConsoleMoveController();
+
+var knight = PieceFactory<string>.ReleasePiece("Knight", "A1", "A1", moveController);
+Console.WriteLine(knight);
+knight.Move("A4");
+Console.WriteLine(knight);
+Console.WriteLine();
+
+var queen = PieceFactory<string>.ReleasePiece("Queen", "A1", "A1", moveController);
+Console.WriteLine(queen);
+queen.Move("B2");
+Console.WriteLine(queen);
+Console.WriteLine();
+
+var bishop = PieceFactory<string>.ReleasePiece("Bishop", "B4", "B4", moveController);
+Console.WriteLine(bishop);
+bishop.Move("E5");
+Console.WriteLine(bishop);
+Console.WriteLine();
+
+var rook = PieceFactory<string>.ReleasePiece("Rook", "C2", "C2", moveController);
+Console.WriteLine(rook);
+rook.Move("C5");
+Console.WriteLine(rook);
+Console.WriteLine();
+
+var king = PieceFactory<string>.ReleasePiece("King", "D4", "D4", moveController);
+Console.WriteLine(king);
+king.Move("E5");
+Console.WriteLine(king);
+Console.WriteLine();
+
+var pawn = PieceFactory<string>.ReleasePiece("Pawn", "B2", "B2", moveController);
+Console.WriteLine(pawn);
+pawn.Move("B3");
+Console.WriteLine(pawn);
